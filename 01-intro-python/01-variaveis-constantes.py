4d8f1ca168fc237a51c8b258c8269da2cd4817f1
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2024-02-28 14:33:50 -0300

    Variáveis e constantes

---
+++ 01-intro-python/01-variaveis-constantes.py
@@ -0,0 +1,50 @@
+# identificadores
+# letra, números e _
+# não pode ser palavra reservada: if, None, True, False
+nome ="Pedro"
+Nome="Pedro"
+
+#variáveis
+#tudo em minusculo
+#separador _
+#snake_case
+
+idade = 20
+pessoa_fisica = "Marcio"
+pessoa_juridica= "Paula LTDA"
+imposto_renda = 2200.45
+
+#E o tipo?
+#Java 
+#String nome="Pedro"
+#int idade = 20
+#No python temos inferencia de tipo
+#O tipo será definido com base no valor (literal)
+
+idade=20 #int
+nome="Pedro" #str
+
+# Constantes 
+# Não existe constante no Python
+# Convenção: nome em maiúsculo 
+PI = 3.14 
+
+#Comentários de única linha
+
+'''
+comentários de
+múltiplas linhas
+'''
+
+# docstring (comentários de documentação)
+# documentar classe, módulos, funções,...
+
+def somar(numero1,numero2):
+    '''
+    Função que realiza a soma de dois números
+    :param numero1: primeiro número
+    :param numero2: segundo número
+    :return: a soma dos números
+    '''
+    return numero1+numero2
+
