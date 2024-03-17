fc2828a51fb9dba9f87707da427bacffdf91210a
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2024-03-17 00:05:41 -0300

    finishing Never seen before exercises

--- Exercicios-ineditos.py/04-identificador-empresa.py
+++ Exercicios-ineditos.py/04-identificador-empresa.py
@@ -1,12 +1,20 @@
+def verify_id(employee_id) :
+
+    valid_lenght = len(employee_id) == 7 
+    valid_start_end = employee_id.startswith("BR") and employee_id.endswith("X") 
+    valid_middle = employee_id[2] != '0' and employee_id[3] != '0' and employee_id[4] != '0' and employee_id[5] !='0'
+
+    valid_id = valid_lenght and valid_start_end and valid_middle
+
+    if valid_id:
+       return "Código válido!"
+    else:
+       return "Código inválido!"
+
 employee_id = input("Entre com o seu identificador, por favor: ")
 
-valid_lenght = len(employee_id) == 7 
-valid_start_end = employee_id.startswith("BR") and employee_id.endswith("X") 
-valid_middle = employee_id[2] != '0' and employee_id[3] != '0' and employee_id[4] != '0' and employee_id[5] !='0'
+validation = verify_id(employee_id)
+print(validation)
 
-valid_id = valid_lenght and valid_start_end and valid_middle
 
-if valid_id:
-    print("Código válido!")
-else:
-    print("Código inválido!")
+
