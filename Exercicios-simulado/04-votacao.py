4283de5e54df43ccacdf2974ab885000c8f34e55
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2024-03-21 10:09:24 -0300

    uiuiu

--- Exercicios-simulado/04-votacao.py
+++ Exercicios-simulado/04-votacao.py
@@ -1,20 +1,18 @@
 print('Vote em um dos 3 candidatos, por favor: ')
 print('Digite as opções correspondentes: ')
 
+votes = [0,0,0]
+
 while True:
     print('1-Candidato 1\n2-Candidato 2\n3-Candidato 3')
     n = int(input())
 
-    c1 = 0
-    c2 = 0
-    c3 = 0
-
     if n == 1:
-        c1+=1
+        votes[0]+=1
     elif n == 2:
-        c2+=1
+        votes[1]+=2
     else:
-        c3+=1
+        votes[2]+=3
     
     print('Deseja continuar a votação? Digite: ')
     keeping_voting = int(input('1-Sim ou 2-Conferir resultados\n'))
@@ -22,15 +20,12 @@ while True:
     if keeping_voting != 1:
         break
 
-votes = [c1,c2,c3]
 max_votes = max(votes)
 
-if c1 == max_votes:
-    print('O candidato ganhador foi: Candidato 1')
-elif c2 == max_votes:
-    print('O candidato ganhador foi: Candidato 2')
-else:
-    print('O candidato ganhador foi: Candidato 3')
+for i in range(len(votes)):
+    print(f'O Candidato {i+1} recebeu {votes[i]} votos.')
 
-    
-
+for i in range(len(votes)):
+        if votes[i] == max_votes:
+            print(f'\nO candidato ganhador foi: Candidato {i+1}')
+            break
