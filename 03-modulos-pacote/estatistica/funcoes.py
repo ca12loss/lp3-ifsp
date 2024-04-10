aae7f5a4e85e45a60eff927ec1129de352747458
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2024-04-10 14:07:17 -0300

    Aula modularização

---
+++ 03-modulos-pacote/estatistica/funcoes.py
@@ -0,0 +1,11 @@
+def media(valores):
+    """
+    Calcula a média dos valores de uma lista
+    """
+    return sum(valores)/len(valores)
+
+def minimo(valores):
+    return min(valores)
+
+def maximo(valores):
+    return max(valores)
---
+++ 03-modulos-pacote/main.py
@@ -0,0 +1,12 @@
+#import funcoes
+
+#from funcoes import media, somar
+
+#from funcoes import *
+
+from funcoes import somar as soma, media
+
+notas = [10.0,3.5,7.5]       
+print(media(notas))
+
+print(soma(7,11))
